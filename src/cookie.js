cda4346b33b8f3e24f8e5947d4d0758097b8621b
Alexy Rumyancev <elder111@yandex.ru>
2017-09-24 22:08:10 +0300

    homework7

---
+++ src/cookie.js
@@ -0,0 +1,93 @@
+/**
+ * ДЗ 7.2 - Создать редактор cookie с возможностью фильтрации
+ *
+ * На странице должна быть таблица со списком имеющихся cookie:
+ * - имя
+ * - значение
+ * - удалить (при нажатии на кнопку, выбранная cookie удаляется из браузера и таблицы)
+ *
+ * На странице должна быть форма для добавления новой cookie:
+ * - имя
+ * - значение
+ * - добавить (при нажатии на кнопку, в браузер и таблицу добавляется новая cookie с указанным именем и значением)
+ *
+ * Если добавляется cookie с именем уже существующией cookie, то ее значение в браузере и таблице должно быть обновлено
+ *
+ * На странице должно быть текстовое поле для фильтрации cookie
+ * В таблице должны быть только те cookie, в имени или значении которых есть введенное значение
+ * Если в поле фильтра пусто, то должны выводиться все доступные cookie
+ * Если дабавляемая cookie не соответсвуте фильтру, то она должна быть добавлена только в браузер, но не в таблицу
+ * Если добавляется cookie, с именем уже существующией cookie и ее новое значение не соответствует фильтру,
+ * то ее значение должно быть обновлено в браузере, а из таблицы cookie должна быть удалена
+ *
+ * Для более подробной информации можно изучить код тестов
+ *
+ * Запрещено использовать сторонние библиотеки. Разрешено пользоваться только тем, что встроено в браузер
+ */
+
+/**
+ * homeworkContainer - это контейнер для всех ваших домашних заданий
+ * Если вы создаете новые html-элементы и добавляете их на страницу, то дабавляйте их только в этот контейнер
+ *
+ * @example
+ * homeworkContainer.appendChild(...);
+ */
+let homeworkContainer = document.querySelector('#homework-container');
+let filterNameInput = homeworkContainer.querySelector('#filter-name-input');
+let addNameInput = homeworkContainer.querySelector('#add-name-input');
+let addValueInput = homeworkContainer.querySelector('#add-value-input');
+let addButton = homeworkContainer.querySelector('#add-button');
+let listTable = homeworkContainer.querySelector('#list-table tbody');
+
+function getCookies() {
+    return document.cookie
+        .split('; ')
+        .filter(Boolean)
+        .map(cookie => cookie.match(/^([^=]+)=(.+)/))
+        .reduce((obj, [, name, value]) => {
+            obj[name] = value;
+
+            return obj;
+        }, {});
+}
+
+function updateCookieTable() {
+    let strFilter = filterNameInput.value;
+    let cookies = getCookies();
+    
+    for (var key in cookies) {
+        if (~key.indexOf(strFilter) || ~cookies[key].indexOf(strFilter)) {
+            let tr = document.createElement('tr');
+
+            tr.innerHTML = `<td>${key}</td>
+                            <td>${cookies[key]}</td>
+                            <td><button class="del-button" data-del = ${key}>Удалить</button></td>`;
+            listTable.appendChild(tr);
+        }
+    }
+}
+
+updateCookieTable();
+
+filterNameInput.addEventListener('keyup', function() {
+    listTable.innerHTML = '';
+    updateCookieTable();
+});
+
+addButton.addEventListener('click', () => {
+    let name = addNameInput.value;
+    let value = addValueInput.value;
+
+    document.cookie = `${name}=${value}`;
+    listTable.innerHTML = '';
+    updateCookieTable();
+
+});
+
+listTable.addEventListener('click', (evt) => {
+    let name = evt.target.getAttribute('data-del');
+
+    document.cookie = name+'='+' '+'; expires =' + new Date(0);
+    listTable.innerHTML = '';
+    updateCookieTable();
+});
